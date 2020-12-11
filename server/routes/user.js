a7f7dab36e8ea546cb4cb21d1796f29ce8c5c9d8
Edmond <ndedmondus97@gmail.com>
2020-12-11 07:47:02 +0200

    ch: Data validation
    check username, email and user password before inserting him/her into db
    check email validity before sending your message
    only allow strong passwords to signup.
    
    [Done]

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { loginUser, signUp } from "../controllers/user";
+import { checkEmail, checkName, checkPassword } from "../helper/validation";
 
 const router = express.Router();
 
 router
-    .post('/signup',signUp)
+    .post('/signup', checkName,checkEmail, checkPassword, signUp)
     .post('/loginuser',loginUser);
     
 
